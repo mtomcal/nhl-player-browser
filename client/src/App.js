d0eece63d8fe35ff0b3aba7fdde5d79ab36b083f
Michael A Tomcal <michael@twoeyetech.com>
2016-03-22 20:12:18 -0700

    Added Pagination

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,66 @@ import {stateStream} from './data/State';
 import './data/Reducers';
 import {PlayerListActions} from './data/ActionCreators';
 import mapComponentToStream from './mapComponentToStream';
+import _ from 'lodash';
 
 export const App = React.createClass({
+  propTypes: {
+    stream: React.PropTypes.object.isRequired
+  },
+  getInitialState() {
+    return {
+      page: 1,
+      perPage: 50
+    };
+  },
+  pageClickHandler(index) {
+    return (e) => {
+      e.preventDefault();
+      this.setState({page: index});
+    };
+  },
+  renderPagination() {
+    const {perPage, page} = this.state;
+    const {playerList} = this.props.stream;
+    const pages = _.range(1, Math.ceil(playerList.length / perPage))
+    .map((index) => {
+      const isActive = (index === page) ? 'active' : '';
+      return <a className={`pager ${isActive}`}
+        href="#"
+        onClick={this.pageClickHandler(index)}>{index}</a>;
+    });
+
+    return (
+      <div className="pagination">
+        {pages}
+      </div>
+    );
+  },
   renderItems() {
+    const {perPage, page} = this.state;
     return this.props.stream.playerList
+    .sort(function (a, b) {
+      if (a.goals < b.goals) {
+        return 1;
+      }
+      if (a.goals > b.goals) {
+        return -1;
+      }
+      if (a.goals === b.goals) {
+        return 0;
+      }
+    })
+    .filter(function (player, index) {
+      return index < (perPage * page) && index > (perPage * page) - perPage;
+    })
     .map(function (player, index) {
       var style = {
         order: -player.goals
       };
       return (
         <div key={`player-${index}`} style={style} className="item card">
+        <img className="mugshot"
+          src={`https://nhl.bamcontent.com/images/headshots/current/168x168/${player.playerId}@2x.jpg`} />
         <h1>{player.playerName}</h1>
         <hr />
         <p>Goals: {player.goals}</p>
@@ -22,8 +72,12 @@ export const App = React.createClass({
   },
   render() {
     return (
-      <div className="container">
-        {this.renderItems()}
+      <div>
+        {this.renderPagination()}
+        <div className="container">
+          {this.renderItems()}
+        </div>
+        {this.renderPagination()}
       </div>
     );
   }
--- client/src/data/Reducers.js
+++ client/src/data/Reducers.js
@@ -11,6 +11,7 @@ const PlayerListStream = Dispatcher
             query {
               players {
                 playerName
+                playerId
                 goals
               }
             }
